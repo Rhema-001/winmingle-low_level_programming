5381f7dcc9ef4a662a1847696cdb931141ea8d62
Rhema-001 <thompsonrhema86@gmail.com>
2026-06-29 18:24:29 +0000

    Add a program to print the alphabet in lower case followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * Author: Thompson Rhema Faith
+  * Program: Winmingle Community C Training
+  * Description: printing alphabet in lower case using only putchar
+  */
+
+int main(void) {
+
+    char X = 'a';
+
+    while (X <= 'z') {
+        putchar(X);
+        X++;
+    }
+    putchar('\n');
+    return(0);
+
+}
